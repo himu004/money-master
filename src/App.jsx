78871d9ab246323ce97c7436c34407ba9079b33b
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-04-27 00:07:12 +0600

    InputBox Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
 
   return (
     <>
       <h1 className='text-center text-yellow-500 bg-green-600'>Currency App</h1>
+      <InputBox/>
     </>
   )
 }
--- src/hooks/useCurrencyinfo.js
+++ src/hooks/useCurrencyinfo.js
@@ -9,7 +9,7 @@ function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((response) => response.json())
         .then(response => setData(response[currency]))
         console.log(data);
@@ -19,6 +19,5 @@ function useCurrencyInfo(currency) {
     return data;
 
 }
-
 
 export default useCurrencyInfo;
