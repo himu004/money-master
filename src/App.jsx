0a32f7c4e6e04f1e7d70f90fa5ca6b02031f3431
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-04-27 00:23:14 +0600

    Main Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 
+import { useState } from 'react'
 import './App.css'
-import InputBox from './components/InputBox'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
+
+  const [amount, setAmount] = useState(0);
+
+  const [from, setFrom] = useState("usd")
+
+  const [to, setTo] = useState("bdt");
+
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+  
 
   return (
     <>
       <h1 className='text-center text-yellow-500 bg-green-600'>Currency App</h1>
-      <InputBox/>
+     <InputBox/>
     </>
   )
 }
